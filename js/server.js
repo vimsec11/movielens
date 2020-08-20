15b99e2f3e6f667184851d944b571b41d310bb1f
vimsec11 <vimsec11@gmail.com>
2020-08-20 17:53:04 +0000

    aggae

--- js/server.js
+++ js/server.js
@@ -1,26 +1,36 @@
 /*eslint no-console: 0, no-unused-vars: 0*/
 "use strict";
 
-var xsjs  = require("@sap/xsjs");
+var xsjs = require("@sap/xsjs");
 var xsenv = require("@sap/xsenv");
-var port  = process.env.PORT || 3000;
+var port = process.env.PORT || 3000;
 
 var options = {
-	anonymous : true, // remove to authenticate calls
-	auditLog : { logToConsole: true }, // change to auditlog service for productive scenarios
-	redirectUrl : "/index.xsjs"
+	//anonymous : true, // remove to authenticate calls
+	auditLog: {
+		logToConsole: true
+	}, // change to auditlog service for productive scenarios
+	redirectUrl: "/index.xsjs"
 };
 
 // configure HANA
 try {
-	options = Object.assign(options, xsenv.getServices({ hana: {tag: "hana"} }));
+	options = Object.assign(options, xsenv.getServices({
+		hana: {
+			tag: "hana"
+		}
+	}));
 } catch (err) {
 	console.log("[WARN]", err.message);
 }
 
 // configure UAA
 try {
-	options = Object.assign(options, xsenv.getServices({ uaa: {tag: "xsuaa"} }));
+	options = Object.assign(options, xsenv.getServices({
+		uaa: {
+			tag: "xsuaa"
+		}
+	}));
 } catch (err) {
 	console.log("[WARN]", err.message);
 }
